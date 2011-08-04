c2c52d29d940cc24e4712b8974cf76f52efca6f8
andrewjpage <andrewjpage@gmail.com>
2011-08-04 16:55:21 +0100

    loop over each pipeline database

---
+++ modules/PipelinesReporting/SendPipelineEmails.pm
@@ -0,0 +1,55 @@
+=head1 NAME
+
+SendPipelineEmails.pm   - Go through each pipeline database and send emails to all studies
+
+=head1 SYNOPSIS
+
+use PipelinesReporting::SendPipelineEmails;
+PipelinesReporting::SendPipelineEmails->new(
+  _qc_dbh => $qc_dbh,
+  pipeline_databases => { db1 => { host => 'example.com', port => 3306, database => 'dbname', user => 'dbusername', password => 'mypassword'} },
+  database_password => 'my optional default password',
+  email_from_address => 'example@example.com',
+  email_domain => 'example.com'
+);
+
+=cut
+
+package PipelinesReporting::SendPipelineEmails;
+use Moose;
+use PipelinesReporting::Schema;
+use PipelinesReporting::SendStudyEmails;
+
+has 'pipeline_databases'     => ( is => 'rw', isa => 'HashRef', required => 1);
+has 'database_password'      => ( is => 'rw', isa => 'Maybe[Str]');
+has '_qc_dbh'                => ( is => 'rw', required   => 1 );
+has 'email_from_address'     => ( is => 'rw', isa => 'Str', required   => 1 );
+has 'email_domain'           => ( is => 'rw', isa => 'Str', required   => 1 );
+
+sub BUILD
+{
+  my ($self) = @_;
+  
+  for my $pipeline_database( %{$self->pipeline_databases})
+  {
+    next if((defined $pipeline_database{disable_emails}) && $pipeline_database{disable_emails} eq 'true');
+    my $database_password = $pipeline_database{password} || $self->database_password;
+    
+    my $pipeline_dbh = PipelinesReporting::Schema->connect(
+      "DBI:mysql:host=$pipeline_database{host}:port=$pipeline_database{port};database=$pipeline_database{database}", 
+      $pipeline_database{user}, $database_password, {'RaiseError' => 1, 'PrintError'=>0}
+    );
+    
+    PipelinesReporting::SendStudyEmails->new(  
+      _pipeline_dbh => $pipeline_dbh,
+      _qc_dbh => $self->_qc_dbh,
+      email_from_address => $self->email_from_address,
+      email_domain => $self->email_domain
+    );
+    
+  }
+
+}
+
+
+1;
---
+++ modules/PipelinesReporting/SendStudyEmails.pm
@@ -0,0 +1,52 @@
+=head1 NAME
+
+SendStudyEmails.pm   - Go through every study and send out emails if nessisary
+
+=head1 SYNOPSIS
+
+use PipelinesReporting::SendStudyEmails;
+my $study = PipelinesReporting::SendStudyEmails->new(  
+  _pipeline_dbh => $pipeline_dbh,
+  _qc_dbh => $qc_dbh,
+  email_from_address => 'example@example.com',
+  email_domain => 'example.com'
+);
+
+=cut
+
+package PipelinesReporting::SendStudyEmails;
+use Moose;
+use PipelinesReporting::Schema;
+use PipelinesReporting::Study;
+
+has '_pipeline_dbh'          => ( is => 'rw',  required   => 1 );
+has '_qc_dbh'                => ( is => 'rw',  required   => 1 );
+has 'email_from_address'     => ( is => 'rw', isa => 'Str', required   => 1 );
+has 'email_domain'           => ( is => 'rw', isa => 'Str', required   => 1 );
+
+sub BUILD
+{
+  my ($self) = @_;
+  my $studies_result_set = $self->_studies_result_sets;
+  while( my $pipeline_study = $studies_result_set->next )
+  {
+    my $study = PipelinesReporting::Study->new(
+      _pipeline_dbh => $self->_pipeline_dbh,
+      _qc_dbh => $self->_qc_dbh,
+      sequencescape_study_id => $pipeline_study->ssid,
+      email_from_address => $self->email_from_address,
+      email_domain => $self->email_domain
+    );
+    $study->send_emails();
+  }
+}
+
+### result sets ###
+sub _studies_result_sets
+{
+  my ($self) = @_;
+  $self->_pipeline_dbh->resultset('Project')->search();
+}
+
+
+1;

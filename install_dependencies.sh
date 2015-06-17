8afb4f88a208c835c0e0b816f16b900d5221dfba
js21 <js21@sanger.ac.uk>
2015-06-17 16:10:31 +0000

    realised I dont need to link the modules folder to PERL5LIB. Will verify if this commit passes ci tests and will do a pull request if it does

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,13 +5,6 @@ set -e
 
 start_dir=$(pwd)
 
-#Add self libs to PERL5LIB for tests to run
-SELF_LIB=${start_dir}'/modules'
-
-export PERL5LIB=${SELF_LIB}:$PERL5LIB
-
-cd $start_dir
-
 cpanm Dist::Zilla
 dzil authordeps --missing | cpanm
 cpanm DBICx::TestDatabase File::Slurp YAML::XS XML::TreePP
